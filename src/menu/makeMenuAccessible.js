e0734d3025dbd5c3f677ed8d90ab4ae1655640a3
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-08 23:37:47 +0100

    added enter and space key press functionality for tabs and menus

--- src/menu/makeMenuAccessible.js
+++ src/menu/makeMenuAccessible.js
@@ -39,6 +39,15 @@ export function makeMenuAccessible(menuId, menuItemClass) {
                     null;
                 triggerButton.focus();
                 break;
+            case 'Enter':
+            case ' ':
+                if (menuItems.item(menuItemIndex).tagName === 'BUTTON') {
+                    menuItems.item(menuItemIndex).click();
+                }
+                else if (menuItems.item(menuItemIndex).tagName === 'A') {
+                    event.preventDefault();
+                    window.location.href = menuItems.item(menuItemIndex).href;
+                }
             default:
                 break;
         }
