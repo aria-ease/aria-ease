3a3480483b964c0d2845862f7c4f4c304054ee2b
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-08 14:14:29 +0100

    added functionality to update menu trigger button aria attributes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { makeMenuAccessible } from './src/menu/makeMenuAccessible.js'
 import { makeTabAccessible } from './src/tab/makeTabAccessible.js'
+import { updateMenuTriggerAriaAttributes } from './src/menu/updateMenuTriggerAriaAttributes.js'
 
 export {
     makeMenuAccessible,
-    makeTabAccessible
+    makeTabAccessible,
+    updateMenuTriggerAriaAttributes
 }
---
+++ src/menu/updateMenuTriggerAriaAttributes.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Updates the aria attributes of the menu trigger button.
+ * @param {string} triggerId The id of the trigger button that toggles the menu.
+ * @param {string} ariaLabel The aria-label to be updated.
+ */
+export declare function updateMenuTriggerAriaAttributes(triggerId: string, ariaLabel: string): void;
