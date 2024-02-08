48dc84db27722c0ae4c5d85af2d5de611bda3092
Scriptkidd <duyilekan98@gmail.com>
2024-02-08 20:22:33 +0100

    fix: changed aria-pressed of checkbox to aria-checked

--- src/checkbox/updateCheckboxAriaAttributes.js
+++ src/checkbox/updateCheckboxAriaAttributes.js
@@ -1,5 +1,5 @@
 /**
- * Adds screen reader accessibility to checkboxes. Updates the aria attributes of the checkbox. Checkbox element must possess the following aria attributes; aria-pressed and aria-label.
+ * Adds screen reader accessibility to checkboxes. Updates the aria attributes of the checkbox. Checkbox element must possess the following aria attributes; aria-checked and aria-label.
  * @param {CheckboxStates[]} checkboxStates Array of objects containing checkboxes state information
  * @param {string} checkboxesClass The shared class of all the checkboxes
  * @param {number} currentPressedCheckboxIndex Index of the currently checked or unchecked checkbox
@@ -11,8 +11,8 @@ export function updateCheckboxAriaAttributes(checkboxStates, checkboxesClass, cu
     }
     allCheckboxes.forEach(function (checkbox, index) {
         if (index === currentPressedCheckboxIndex) {
-            checkbox.setAttribute("aria-pressed", checkboxStates[index].pressed ? 'true' : 'false');
-            checkbox.setAttribute("aria-label", checkboxStates[index].pressed ? checkboxStates[index].checkedAriaLabel : checkboxStates[index].uncheckedAriaLabel);
+            checkbox.setAttribute("aria-checked", checkboxStates[index].checked ? 'true' : 'false');
+            checkbox.setAttribute("aria-label", checkboxStates[index].checked ? checkboxStates[index].checkedAriaLabel : checkboxStates[index].uncheckedAriaLabel);
         }
     });
 }
