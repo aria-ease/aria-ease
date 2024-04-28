c2c2a7988a3d5ccf919e3fef33a3609179082fdf
Isaac Akinduyile <scriptkidd98@gmail.com>
2024-04-28 23:29:14 +0100

    feat: added support for toggle buttons component. fix: abstracted checkbox and radio functionalities to single and group

--- src/toggle/group/updateGroupTogglesAriaAttributes.js
+++ src/toggle/group/updateGroupTogglesAriaAttributes.js
@@ -1,8 +1,8 @@
 /**
- * Adds screen reader accessibility to toggle elements. Updates the aria attributes of the toggle elements. Toggle element must possess the following aria attributes; aria-pressed and aria-label.
- * @param {ToggleStates[]} toggleStates Array of objects containing toggle elements state information
- * @param {string} togglesClass The shared class of all the toggle elements
- * @param {number} currentPressedToggleIndex Index of the currently pressed or unpressed toggle element
+ * Adds screen reader accessibility to toggle buttons. Updates the aria attributes of the toggle buttons. Button must be button element with a role of button, and possess the following aria attributes; aria-pressed and aria-label.
+ * @param {ToggleStates[]} toggleStates Array of objects containing toggle buttons state information
+ * @param {string} togglesClass The shared class of all the toggle buttons
+ * @param {number} currentPressedToggleIndex Index of the currently pressed or unpressed toggle button
  */
 export function updateGroupTogglesAriaAttributes(toggleStates, togglesClass, currentPressedToggleIndex) {
     var allToggles = Array.from(document.querySelectorAll(".".concat(togglesClass)));
